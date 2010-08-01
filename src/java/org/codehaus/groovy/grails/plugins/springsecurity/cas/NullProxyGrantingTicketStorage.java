174209979931a50f5d56f7a45c228548550540cc
Burt Beckwith <burt@burtbeckwith.com>
2010-08-01 02:41:19 -0400

    removed @Override annotations on interface methods that caused compilation errors in jdk 5

--- src/java/org/codehaus/groovy/grails/plugins/springsecurity/cas/NullProxyGrantingTicketStorage.java
+++ src/java/org/codehaus/groovy/grails/plugins/springsecurity/cas/NullProxyGrantingTicketStorage.java
@@ -27,7 +27,6 @@ public class NullProxyGrantingTicketStorage implements ProxyGrantingTicketStorag
 	 * {@inheritDoc}
 	 * @see org.jasig.cas.client.proxy.ProxyGrantingTicketStorage#cleanUp()
 	 */
-	@Override
 	public void cleanUp() {
 		// do nothing	
 	}
@@ -36,7 +35,6 @@ public class NullProxyGrantingTicketStorage implements ProxyGrantingTicketStorag
 	 * {@inheritDoc}
 	 * @see org.jasig.cas.client.proxy.ProxyGrantingTicketStorage#retrieve(java.lang.String)
 	 */
-	@Override
 	public String retrieve(final String proxyGrantingTicketIou) {
 		return null;
 	}
@@ -46,7 +44,6 @@ public class NullProxyGrantingTicketStorage implements ProxyGrantingTicketStorag
 	 * @see org.jasig.cas.client.proxy.ProxyGrantingTicketStorage#save(
 	 * 	java.lang.String, java.lang.String)
 	 */
-	@Override
 	public void save(final String proxyGrantingTicketIou, final String proxyGrantingTicket) {
 		// do nothing	
 	}
