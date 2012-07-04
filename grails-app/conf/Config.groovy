b3eeb97d5f5137d33d7ff9c7568fb74888e5a415
Burt Beckwith <beckwithb@vmware.com>
2012-07-04 01:11:03 -0400

    cleanup

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -7,4 +7,4 @@ grails.plugins.springsecurity.cas.serverUrlPrefix = 'http://localhost:9090/cas-s
 
 grails.doc.authors = 'Burt Beckwith'
 grails.doc.license = 'Apache License 2.0'
-
+grails.doc.title = 'Spring Security ACL Plugin'
