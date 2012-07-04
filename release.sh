d5ed8afce66a3a0592b37713cb7f7d3300ea2474
Burt Beckwith <beckwithb@vmware.com>
2012-07-04 01:19:14 -0400

    fixed plugin name

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 rm -rf target/release
 mkdir target/release
 cd target/release
-git clone git@github.com:grails-plugins/grails-spring-security-acl.git
-cd grails-spring-security-acl
+git clone git@github.com:grails-plugins/grails-spring-security-cas.git
+cd grails-spring-security-cas
 grails clean
 grails compile
 grails publish-plugin --noScm --stacktrace
