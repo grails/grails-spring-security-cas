3b5ccd7f9dd01a069950e7994b259035f1348654
Burt Beckwith <burt@burtbeckwith.com>
2015-12-01 15:59:50 -0500

    updated to Spring Security 3.2.9.RELEASE

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,7 @@
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for backwards-compatibility, the docs are checked into gh-pages branch
 
-grails.project.dependency.resolver="maven"
+grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
 
 	inherits 'global'
@@ -11,28 +11,27 @@ grails.project.dependency.resolution = {
 		grailsCentral()
 		mavenLocal()
 		mavenCentral()
-		mavenRepo 'http://repo.spring.io/milestone' // TODO remove
 	}
 
 	dependencies {
-		String springSecurityVersion = '3.2.7.RELEASE'
+		String springSecurityVersion = '3.2.9.RELEASE'
 
 		compile "org.springframework.security:spring-security-cas:$springSecurityVersion", {
-			excludes 'cas-client-core', 'commons-logging', 'ehcache', 'fest-assert', 'jcl-over-slf4j', 'junit',
+			excludes 'cas-client-core', 'commons-logging', 'ehcache-core', 'fest-assert', 'jcl-over-slf4j', 'junit',
 			         'logback-classic', 'mockito-core', 'spring-beans', 'spring-context', 'spring-core',
-			         'spring-security-core', 'spring-security-web', 'spring-test', 'spring-web', 'tomcat-servlet-api'
+			         'spring-security-core', 'spring-security-web', 'spring-test', 'spring-web', 'javax.servlet-api'
 		}
 
-		compile 'org.jasig.cas.client:cas-client-core:3.3.3', {
-			excludes 'commons-codec', 'commons-logging', 'junit', 'log4j', 'opensaml', 'servlet-api',
-			         'spring-beans', 'spring-context', 'spring-core', 'spring-test', 'xmlsec'
+		compile 'org.jasig.cas.client:cas-client-core:3.4.1', {
+			excludes 'commons-codec', 'javax.servlet-api', 'jcl-over-slf4j', 'junit', 'log4j', 'slf4j-api',
+			         'slf4j-simple', 'spring-beans', 'spring-context', 'spring-core', 'spring-test', 'xmlsec'
 		}
 	}
 
 	plugins {
-		compile ':spring-security-core:2.0-RC5'
+		compile ':spring-security-core:2.0-RC6'
 
-		build (":release:3.1.1", ":rest-client-builder:2.1.1") {
+		build ':release:3.1.2', ':rest-client-builder:2.1.1', {
 			export = false
 		}
 	}
