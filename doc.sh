ecad9f828dbd8026f575d0317eefee54dc1358f1
Burt Beckwith <burt@burtbeckwith.com>
2015-12-30 19:42:23 -0500

    standardized .gitignore, build.gradle, and gradle.properties across plugins using common includes updated to Grails v3.0.11, Gradle 2.10 minor build cleanup

--- doc.sh
+++
@@ -1,2 +0,0 @@
-rm -rf docs
-gradle --stacktrace docs
--- release.sh
+++
@@ -1,9 +0,0 @@
-rm -rf build/release
-mkdir -p build/release
-cd build/release
-git clone git@github.com:grails-plugins/grails-spring-security-cas.git
-cd grails-spring-security-cas
-grails clean
-grails compile
-
-gradle bintrayUpload --stacktrace
